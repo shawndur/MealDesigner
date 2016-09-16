4740913708cf7b98432ea1e83f0b6318295906cb
shawn <shawndur@buffalo.edu>
2016-09-15 23:29:38 -0400

    Made the cook fragment extend main fragment

--- app/src/main/java/letseat/mealdesigner/CookFragment.java
+++ app/src/main/java/letseat/mealdesigner/CookFragment.java
@@ -11,7 +11,7 @@ import android.view.ViewGroup;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class CookFragment extends Fragment {
+public class CookFragment extends MainFragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
