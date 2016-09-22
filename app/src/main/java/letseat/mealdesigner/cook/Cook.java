de9810e24f1c064129069d2e510725f826a9cddd
shawn <shawndur@buffalo.edu>
2016-09-21 22:23:39 -0400

    Prototype 2.0, I redesigned the prototype

---
+++ app/src/main/java/letseat/mealdesigner/cook/Cook.java
@@ -0,0 +1,51 @@
+package letseat.mealdesigner.cook;
+
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import letseat.mealdesigner.R;
+
+public class Cook extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cook);
+
+        //set toolbar widget as action bar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle(getString(R.string.navdrawer_cook));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.toolbar_cook, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //handle toolbar button presses
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                //back button pressed, navigate to main task
+               onBackPressed();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /*@Override
+    public void onBackPressed() {
+        //handle navbar back button press
+        NavUtils.navigateUpFromSameTask(this);
+    }*/
+}
