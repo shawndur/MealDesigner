50edf84b8911651e836f438ae00c6be2a06dff22
shawndur <shawndur@gmail.com>
2016-09-24 18:03:56 -0400

    Prototype 2.0 (#13)
    * Prototype 2.0, I redesigned the prototype
    
    * Added buttons to demonstrate movement through the app
    
    * Added example toolbar buttons and set main activity

---
+++ app/src/main/java/letseat/mealdesigner/shoppinglist/ShoppingList.java
@@ -0,0 +1,62 @@
+package letseat.mealdesigner.shoppinglist;
+
+import android.content.Intent;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import letseat.mealdesigner.R;
+import letseat.mealdesigner.favorites.Favorites;
+import letseat.mealdesigner.recipeinfo.RecipeInfo;
+
+public class ShoppingList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shopping_list);
+
+        //set toolbar widget as action bar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle(getString(R.string.navdrawer_list));
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.toolbar_shoppinglist, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //handle toolbar button presses
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                //back button pressed, navigate to main task
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        //handle navbar back button press
+        NavUtils.navigateUpFromSameTask(this);
+    }
+
+    public void openRecipeInfo(View view){
+        Intent intent = new Intent(this,RecipeInfo.class);
+        startActivity(intent);
+    }
+
+}
