aacfa0af57a80344c3064ca585bd849e0e483455
changpark <pcm4150@gmail.com>
2016-09-24 22:10:53 -0400

    rest of changes(Accidently didn't commit) belongs to previous commit

---
+++ app/src/main/java/letseat/mealdesigner/ShoppingList.java
@@ -0,0 +1,19 @@
+package letseat.mealdesigner;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import letseat.mealdesigner.recipeinfo.RecipeInfo;
+
+public class ShoppingList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shopping_list);
+    }
+
+
+}
