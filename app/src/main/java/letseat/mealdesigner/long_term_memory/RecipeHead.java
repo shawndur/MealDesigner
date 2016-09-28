1698934c859dca1a0286048dd34d790b65dc5d31
vanhoose <vanhoose@buffalo.edu>
2016-09-28 10:15:44 -0400

    Continued working on RecipeHead class, implemented functions to add ListComponent nodes to the respective lists in the RecipeHead member variables.

--- app/src/main/java/letseat/mealdesigner/long_term_memory/RecipeHead.java
+++ app/src/main/java/letseat/mealdesigner/long_term_memory/RecipeHead.java
@@ -21,67 +21,83 @@ public class RecipeHead
 
     public void addEquipment(String equipment_name, double quantity_needed, ArrayList<String> comments)
     {
-        ListComponent toAdd = new ListComponent(equipment_name, quantity_needed, comments);
+        ListComponent equipmentComponent = new ListComponent(equipment_name, quantity_needed, comments);
 
         _equiCount++;
 
         if(_equipmentList == null)
         {
-            _equiLast = _equipmentList = toAdd;
+            _equiLast = _equipmentList = equipmentComponent;
 
 
         }
         else
         {
-            _equiLast.setNext(toAdd);
+            _equiLast = _equiLast.setNext(equipmentComponent);
         }
     }
 
     public void addIngredient(String name, double quantity, String preparation_procedure, ArrayList<String> comments, ListComponent.UnitOfMeasure unit_of_measure)
     {
-        ListComponent toAdd = new ListComponent(name, quantity, preparation_procedure, comments, unit_of_measure);
+        ListComponent ingredientComponent = new ListComponent(name, quantity, preparation_procedure, comments, unit_of_measure);
 
         _ingrCount++;
 
         if(_ingredientList == null)
         {
-            _ingrLast = _ingredientList = toAdd;
+            _ingrLast = _ingredientList = ingredientComponent;
         }
         else
         {
-            _equiLast.setNext(toAdd);
+            _ingrLast = _ingrLast.setNext(ingredientComponent);
         }
     }
 
     public void addProcedureWithTimer(String name, double timer_in_seconds, String hazards_and_critical_control_points, ArrayList<String> comments)
     {
-        ListComponent toAdd = new ListComponent(name, timer_in_seconds, hazards_and_critical_control_points, comments);
+        ListComponent procedureComponentWithTimer = new ListComponent(name, timer_in_seconds, hazards_and_critical_control_points, comments);
 
         _procCount++;
 
         if(_procedureList == null)
         {
-            _procLast = _procedureList = toAdd;
+            _procLast = _procedureList = procedureComponentWithTimer;
         }
         else
         {
-            _procLast.setNext(toAdd);
+            _procLast = _procLast.setNext(procedureComponentWithTimer);
         }
     }
 
     public void addProcedureWithoutTimer(String name, String hazards_and_critical_control_points, ArrayList<String> comments)
     {
-        ListComponent toAdd = new ListComponent(name, hazards_and_critical_control_points, comments);
+        ListComponent procedureComponentWithoutTimer = new ListComponent(name, hazards_and_critical_control_points, comments);
 
         _procCount++;
 
         if(_procedureList == null)
         {
-            _procLast = _procedureList = toAdd;
+            _procLast = _procedureList = procedureComponentWithoutTimer;
         }
         else
         {
-            _procLast.setNext(toAdd);
+            _procLast = _procLast.setNext(procedureComponentWithoutTimer);
+        }
+    }
+
+    public void addComment(String text)
+    {
+        ListComponent commentComponent = new ListComponent(text);
+
+        _commCount++;
+
+        if(_commentList == null)
+        {
+            _commLast = _commentList = commentComponent;
+        }
+        else
+        {
+            _commLast = _commLast.setNext(commentComponent);
         }
     }
 
