0a61eaf958981adf0ba4c2380efb3aa4125b7ea8
shawndur <shawndur@gmail.com>
2016-09-28 12:33:28 -0400

    Created functions to handle button presses and retrieving of recipe info
     Added code to onCreate to initialize text views

--- app/src/main/java/letseat/mealdesigner/recipeinfo/RecipeInfo.java
+++ app/src/main/java/letseat/mealdesigner/recipeinfo/RecipeInfo.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
 import letseat.mealdesigner.R;
 import letseat.mealdesigner.cook.Cook;
@@ -25,6 +26,14 @@ public class RecipeInfo extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle(getString(R.string.recipe_info));
+
+        //set text views with recipe info
+        TextView text  = (TextView) findViewById(R.id.recipe_name_text);
+        text.setText(getRecipeName());
+        text  = (TextView) findViewById(R.id.recipe_steps_text);
+        text.setText(getSteps());
+        text  = (TextView) findViewById(R.id.recipe_ingredients_text);
+        text.setText(getIngredients());
     }
 
     @Override
@@ -42,12 +51,44 @@ public class RecipeInfo extends AppCompatActivity {
             case android.R.id.home:
                 //back button pressed, navigate to main task
                 onBackPressed();
-                return true;
+                break;
+            case R.id.toolbar_fav:
+                addToFavs();
+                break;
+            case R.id.toolbar_list:
+                addToList();
+                break;
+            case R.id.toolbar_cook:
+                goToCook();
+                break;
         }
         return super.onOptionsItemSelected(item);
     }
 
-    public void startCooking(View view){
+    public String getRecipeName(){
+        String result = "";
+        return result;
+    }
+
+    public String getSteps(){
+        String result = "";
+        return result;
+    }
+
+    public String getIngredients(){
+        String result = "";
+        return result;
+    }
+
+    public void addToList(){
+
+    }
+
+    public void addToFavs(){
+
+    }
+
+    public void goToCook(){
         Intent intent = new Intent(this,Cook.class);
         startActivity(intent);
     }
