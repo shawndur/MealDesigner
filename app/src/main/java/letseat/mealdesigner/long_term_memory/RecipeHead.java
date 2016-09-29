b1b91d153af3728735bf91b402118f837063708a
vanhoose <vanhoose@buffalo.edu>
2016-09-28 22:36:20 -0400

    Continued working on RecipeHead class, implemented functions to add ListComponent nodes to the respective lists in the RecipeHead member variables.

--- app/src/main/java/letseat/mealdesigner/long_term_memory/RecipeHead.java
+++ app/src/main/java/letseat/mealdesigner/long_term_memory/RecipeHead.java
@@ -14,7 +14,7 @@ import letseat.mealdesigner.long_term_memory.ListComponent.ComponentType;
 
 public class RecipeHead
 {
-//    private ListComponent _equipmentList, _equiLast, _ingredientList, _ingrLast, _procedureList, _procLast, _commentList, _commLast;
+    //    private ListComponent _equipmentList, _equiLast, _ingredientList, _ingrLast, _procedureList, _procLast, _commentList, _commLast;
 //    private int _equiCount, _ingrCount, _procCount, _commCount;
     private Map<ComponentType, Integer> _counts;
     private Map<ComponentType, ListComponent> _heads, _tails;
@@ -22,11 +22,13 @@ public class RecipeHead
 
 
 
-    public RecipeHead()
+    public RecipeHead(String recipeName)
     {
 //        _equipmentList = _equiLast = _ingredientList = _ingrLast = _procedureList = _procLast = _commentList = _commLast = null;
 
 //        _equiCount = _ingrCount = _procCount = _commCount = 0;
+
+        _recipeName = recipeName;
 
         _counts = new HashMap<ComponentType,Integer>();
 
@@ -38,7 +40,17 @@ public class RecipeHead
 
     private int modifyComponentCount(final ListComponent component, int change)
     {
-        return _counts.put(component.getComponentType(), _counts.get(component.getComponentType()) + change);
+        // calling this function once makes more sense than calling it a bunch of times in the return line.
+        ListComponent.ComponentType componentType = component.getComponentType();
+
+        // gonna make that compiler work!
+        // this return statement first checks to ensure that _counts actually contains a non-null KV pair for the given component,
+        // 		if true, it adds the incoming change to the existing value then places that value back in the key
+        //		if false, it creates a key for the component then sets its value to change, but only if change is positive or zero; if change is negative it sets the key's value to zero.
+        // It then checks if the displaced KV pair was null
+        //		if null, it returns change if change is positive or zero, or zero if change is negative (you can't have -3 pans in a recipe, now can you?)
+        //		if non-null, it checks if the displaced value was positive or zero and returns that value plus change, or in the case of a negative value in (change plus the displaced KV pair), returns zero.
+        return (_counts.put(componentType, (_counts.containsKey(componentType))? _counts.get(componentType) + change : (change >= 0)? change : 0) == null)? ((change >= 0)? change : 0) : ((_counts.get(componentType) + change) >= 0)? (_counts.get(componentType) + change) : 0;
     }
 
     private int decrementComponentCount(final ListComponent component)
@@ -51,17 +63,21 @@ public class RecipeHead
         return modifyComponentCount(component,1);
     }
 
-    private ListComponent updateFirstAndLast(ListComponent node)
+    private void updateFirstAndLast(ListComponent node)
     {
         ComponentType nodeType = node.getComponentType();
 
         if(incrementComponentCount(node) == 1)
         {
-            return _heads.put(nodeType, _tails.put(nodeType, _heads.get(nodeType)));
+
+            _heads.put(nodeType, node);
+
+            _tails.put(nodeType, node);
+
+            return;	// I think this was causing the stack overflow...  Bingo!!!
         }
 
-        return _tails.put(nodeType, _tails.get(nodeType).setNext(node));
-
+        _tails.put(nodeType, node).setNext(node);	// this is the problem right here!
     }
 
     public void addEquipment(String equipment_name, double quantity_needed, ArrayList<String> comments)
@@ -173,5 +189,60 @@ public class RecipeHead
         return _counts.get(componentType);
     }
 
+//    public void sortComponents(ComponentType componentType)
+//    {
+//        _tails.remove(componentType);
+//
+//        _heads
+//    }
+
+
+    /**
+     * A debugging method
+     */
+    public void printAll()
+    {
+        if(_heads.containsKey(ComponentType.EQUIPMENT))
+        {
+            _heads.get(ComponentType.EQUIPMENT).printAllInfo();
+        }
+
+        if(_heads.containsKey(ComponentType.INGREDIENT))
+        {
+            _heads.get(ComponentType.INGREDIENT).printAllInfo();
+        }
+
+        if(_heads.containsKey(ComponentType.PROCEDURE))
+        {
+            _heads.get(ComponentType.PROCEDURE).printAllInfo();
+        }
+
+        if(_heads.containsKey(ComponentType.PROCEDURE_WITH_TIMER))
+        {
+            _heads.get(ComponentType.PROCEDURE_WITH_TIMER).printAllInfo();
+        }
+
+        if(_heads.containsKey(ComponentType.COMMENT))
+        {
+            _heads.get(ComponentType.COMMENT).printAllInfo();
+        }
+
+    }
+
+    /**
+     * a debugging method
+     */
+    public void println(String printable)
+    {
+        System.out.println(printable);
+    }
+
+    /**
+     * a debugging method
+     */
+    public void print(String printable)
+    {
+        System.out.print(printable);
+    }
 
 }
