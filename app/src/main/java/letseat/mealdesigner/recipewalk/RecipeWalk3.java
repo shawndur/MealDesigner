76f149c0b3c6ea00f032d469f3ba2ded4a1354d5
cnzytari <cnzytari@buffalo.edu>
2016-09-30 14:46:28 -0400

    added in java nad xml code for button clicks for pages 1-3

--- app/src/main/java/letseat/mealdesigner/recipewalk/RecipeWalk3.java
+++ app/src/main/java/letseat/mealdesigner/recipewalk/RecipeWalk3.java
@@ -1,7 +1,40 @@
 package letseat.mealdesigner.recipewalk;
 
-/**
- * Created by Christina on 9/30/2016.
- */
-public class RecipeWalk3 {
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import letseat.mealdesigner.R;
+import letseat.mealdesigner.recipeinfo.RecipeInfo;
+import letseat.mealdesigner.recipewalk.RecipeWalk4;
+import letseat.mealdesigner.favorites.Favorites;
+import letseat.mealdesigner.recipies.MainRecipe;
+
+public class RecipeWalk3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe_walk2);
+
+        //has no toolbar
+
+    }
+
+    //next step button
+    public void step4(View view){
+        Intent intent = new Intent(this,RecipeWalk4.class);
+        startActivity(intent);
+    }
+    //exit button back to main recipe page
+    public void exit(View view){
+        Intent intent = new Intent(this,MainRecipe.class);
+        startActivity(intent);
+    }
+
+
 }
