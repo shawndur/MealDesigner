8cde1a819816e50c487ad3b9b2ce22d0421bd7d4
cnzytari <cnzytari@buffalo.edu>
2016-10-03 10:46:20 -0400

    finished button setup and java code for walk 4-6, added activities into Manifest test ran and appeared to work

--- app/src/main/java/letseat/mealdesigner/recipewalk/RecipeWalk6.java
+++ app/src/main/java/letseat/mealdesigner/recipewalk/RecipeWalk6.java
@@ -1,7 +1,41 @@
+
+
 package letseat.mealdesigner.recipewalk;
 
-/**
- * Created by Christina on 9/30/2016.
- */
-public class RecipeWalk6 {
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import letseat.mealdesigner.R;
+import letseat.mealdesigner.recipeinfo.RecipeInfo;
+import letseat.mealdesigner.recipewalk.RecipeWalk6;
+import letseat.mealdesigner.recipies.MainRecipe;
+
+public class RecipeWalk6 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe_walk6);
+
+        //has no toolbar
+
+    }
+
+    //exit button back to main recipe page
+    public void exit(View view){
+        Intent intent = new Intent(this,MainRecipe.class);
+        startActivity(intent);
+    }
+    //button to created recipe page
+    public void finalCreate(View view){
+        Intent intent = new Intent(this,MainRecipe.class);
+        startActivity(intent);
+    }
+
+
 }
