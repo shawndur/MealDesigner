aea88e6cd7537c2714935cd7ca9aa6fe11084325
cnzytari <cnzytari@buffalo.edu>
2016-10-06 19:06:28 -0400

    added drop down spinner for equipment and measuring units, removed some unnecessary imports causing warnings, added additional button to main recipe content

--- app/src/main/java/letseat/mealdesigner/recipewalk/RecipeWalk2.java
+++ app/src/main/java/letseat/mealdesigner/recipewalk/RecipeWalk2.java
@@ -3,18 +3,20 @@ package letseat.mealdesigner.recipewalk;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.AdapterView.OnItemSelectedListener;
+
 
 import letseat.mealdesigner.R;
-import letseat.mealdesigner.recipeinfo.RecipeInfo;
-import letseat.mealdesigner.recipewalk.RecipeWalk3;
-import letseat.mealdesigner.favorites.Favorites;
+
 import letseat.mealdesigner.recipies.MainRecipe;
 
 public class RecipeWalk2 extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +24,34 @@ public class RecipeWalk2 extends AppCompatActivity {
         setContentView(R.layout.activity_recipe_walk2);
 
         //has no toolbar
+
+        //declare spinner
+        Spinner equip_spinner = (Spinner) findViewById(R.id.equip_spinner);
+
+        //populate spinner
+        String[] items = new String[] { "Fork", "Spoon", "Knife", "Measuring Cup", "Skillet", "Large Pot", "Strainer" };
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, items);
+
+        // Specify the layout to be more spaced out
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        //apply the adapter
+        equip_spinner.setAdapter(adapter);
+
+        equip_spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
+            @Override //what to do with selected item
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Log.v("item", (String) parent.getItemAtPosition(position));
+            }
+
+            @Override //what to do if no item selected
+            public void onNothingSelected(AdapterView<?> parent) {
+                // TODO Auto-generated method stub
+            }
+        });
+
 
     }
 
--- app/src/main/java/letseat/mealdesigner/recipewalk/RecipeWalk6.java
+++ app/src/main/java/letseat/mealdesigner/recipewalk/RecipeWalk6.java
@@ -5,14 +5,9 @@ package letseat.mealdesigner.recipewalk;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 
 import letseat.mealdesigner.R;
-import letseat.mealdesigner.recipeinfo.RecipeInfo;
-import letseat.mealdesigner.recipewalk.RecipeWalk6;
 import letseat.mealdesigner.recipies.MainRecipe;
 
 public class RecipeWalk6 extends AppCompatActivity {
