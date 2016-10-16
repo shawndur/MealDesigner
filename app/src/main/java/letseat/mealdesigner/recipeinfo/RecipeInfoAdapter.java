f53f6de574ca70ac40b721f309a022bb25d08ed4
shawn <shawndur@buffalo.edu>
2016-10-15 22:32:45 -0400

    Cleaned up and commented code

--- app/src/main/java/letseat/mealdesigner/recipeinfo/RecipeInfoAdapter.java
+++ app/src/main/java/letseat/mealdesigner/recipeinfo/RecipeInfoAdapter.java
@@ -8,33 +8,46 @@ import android.widget.TextView;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-
 import letseat.mealdesigner.R;
 
 /**
  * Created by shawn on 10/14/16.
+ * This is the Adapter class for the Recycler view used in recipe info.
+ * This is used to create and modify the views for the recipe components
  */
 
 class RecipeInfoAdapter extends RecyclerView.Adapter<RecipeInfoAdapter.ViewHolder> {
 
-    private Map<String,List<String>> _dataset;
+    private Map<String,List<String>> _dataset; //holds names and values of components
 
-    // Provide a reference to the views for each data item
-    // Complex data items may need more than one view per item, and
-    // you provide access to all the views for a data item in a view holder
+    /**
+     * Holds and maintains references to views
+     */
     static class ViewHolder extends RecyclerView.ViewHolder {
-        // each data item is just a string in this case
-        private TextView _header,_text;
+        private TextView _header,_text; //textviews used to display recipe info
+
+        /**
+         * Saves references to the views and subviews
+         * @param v view to be referenced
+         */
         ViewHolder(View v) {
             super(v);
             _header = (TextView) v.findViewById(R.id.info_item_header);
             _text = (TextView) v.findViewById(R.id.info_item_text);
         }
 
+        /**
+         * Sets the header for the recipe component
+         * @param header String value of the header
+         */
         void setHeader(String header){
             _header.setText(header);
         }
 
+        /**
+         * Sets the text of the recipe component
+         * @param text String value of the text
+         */
         void setText(List<String> text){
             _text.setText("");
             for(String s : text){
@@ -43,12 +56,20 @@ class RecipeInfoAdapter extends RecyclerView.Adapter<RecipeInfoAdapter.ViewHolde
         }
     }
 
-    // Provide a suitable constructor (depends on the kind of dataset)
+    /**
+     * Constructor for the adapter, stores the dataset
+     * @param myDataset Dataset to be used for the components
+     */
     RecipeInfoAdapter(Map<String,List<String>> myDataset) {
         _dataset = myDataset;
     }
 
-    // Create new views (invoked by the layout manager)
+    /**
+     * Called by the layout manager to create a view
+     * @param parent ViewGroup to where the view is inflated
+     * @param viewType Type of the new view
+     * @return A newly created view holder
+     */
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         // create a new view
@@ -57,20 +78,29 @@ class RecipeInfoAdapter extends RecyclerView.Adapter<RecipeInfoAdapter.ViewHolde
         return new ViewHolder(v);
     }
 
-    // Replace the contents of a view (invoked by the layout manager)
+    /**
+     * Called by the layout manager to set/update views
+     * @param holder View holder holding the views to be modified
+     * @param position Position in the list/dataset
+     */
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
+        //get iterator at position i
         Iterator<Map.Entry<String,List<String>>> it = _dataset.entrySet().iterator();
         for(int i=0;i<position && it.hasNext();++i ) it.next();
 
         if(it.hasNext()) {
+            //get key value pair at position and update in viewholder
             Map.Entry<String, List<String>> entry = it.next();
             holder.setHeader(entry.getKey());
             holder.setText(entry.getValue());
         }
     }
 
-    // Return the size of your dataset (invoked by the layout manager)
+    /**
+     * Used by the layout manager to get the size of the recycler view
+     * @return The size of the dataset
+     */
     @Override
     public int getItemCount() {
         return _dataset.size();
