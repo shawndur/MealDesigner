89cd847e427368108a09144b228ad18850bb3a11
cnzytari <cnzytari@buffalo.edu>
2016-10-23 16:09:29 -0400

    saving to database recipe walk page 6

--- app/src/main/java/letseat/mealdesigner/recipewalk/RecipeWalk6.java
+++ app/src/main/java/letseat/mealdesigner/recipewalk/RecipeWalk6.java
@@ -6,13 +6,26 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+
+import java.util.ArrayList;
 
 import letseat.mealdesigner.MealDesignerApp;
 import letseat.mealdesigner.R;
 import letseat.mealdesigner.recipies.MainRecipe;
 import letseat.mealdesigner.storage.Database;
+import letseat.mealdesigner.storage.Recipe;
 
 public class RecipeWalk6 extends AppCompatActivity {
+    private EditText editText1;
+    private EditText editText2;
+    private EditText editText3;
+    private EditText editText4;
+    private EditText editText5;
+    ArrayList<String> allergies  = new ArrayList<>();
+    ArrayAdapter<String> m_adapter;
+    Recipe newRecipe;
     Database x = ((MealDesignerApp) getApplication()).getDatabase();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +34,18 @@ public class RecipeWalk6 extends AppCompatActivity {
 
         //has no toolbar
 
+        editText1 = (EditText) findViewById(R.id.editText8);
+        editText2 = (EditText) findViewById(R.id.editText9);
+        editText3 = (EditText) findViewById(R.id.editText10);
+        editText4 = (EditText) findViewById(R.id.editText11);
+        editText5 = (EditText) findViewById(R.id.editText12);
     }
+
+    final String input1 = editText1.getText().toString();
+    final String input2 = editText2.getText().toString();
+    final String input3 = editText3.getText().toString();
+    final String input4 = editText4.getText().toString();
+    final String input5 = editText5.getText().toString();
 
     //exit button back to main recipe page
     public void exit(View view){
@@ -30,6 +54,11 @@ public class RecipeWalk6 extends AppCompatActivity {
     }
     //button to created recipe page
     public void finalCreate(View view){
+        allergies.add(input1);
+        allergies.add(input2);
+        allergies.add(input3);
+        allergies.add(input4);
+        newRecipe.setSteps(allergies);
         Intent intent = new Intent(this,MainRecipe.class);
         startActivity(intent);
     }
